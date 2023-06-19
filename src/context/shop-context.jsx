38000bb6726233d1f552dc9738b0bda6620c6b06
Nabeel Khan <nabeel.kay@gmail.com>
2023-06-19 16:18:22 +0100

    wip

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -14,6 +14,18 @@ const getDefaultCart=()=>{
 const ShopContextProvider = (props) => {
   const[cartItems,SetCartItems]=useState(getDefaultCart())
 
+  const getTotalCartAmount=()=>{
+    let totalAmount=0
+    //Below is how to loop through an object rather than array
+    for(const item in cartItems){
+      if(cartItems[item]>0){
+        let itemInfo=PRODUCTS.find((product)=>product.id===Number(item))
+        totalAmount+=cartItems[item]*itemInfo.price
+      }
+    }
+    return totalAmount
+  }
+
   const addToCart=(itemId)=>{
 SetCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
   }
@@ -21,7 +33,11 @@ SetCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
 SetCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
   }
 
-const contextValue={cartItems,addToCart,removeFromCart}
+  const updateCartItemAmount=(newAmount,itemId)=>{
+    SetCartItems((prev)=>({...prev,[itemId]:newAmount}))
+
+  }
+const contextValue={cartItems,addToCart,removeFromCart,updateCartItemAmount,getTotalCartAmount}
 
   return (
     <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>
